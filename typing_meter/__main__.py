32bcd00f2a756ce039558d938b452c9ad5694d5d
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2022-04-18 15:19:22 +0530

    Working Copy

--- typing_meter/__main__.py
+++ typing_meter/__main__.py
@@ -1,12 +1,6 @@
 """typing_meter."""
 
 from app import App
-choice = input("Enter the language you want to practise on: (English/Hindi): ")
-if choice == 'Hindi':
-    pass
-else:
-    mode = input("Enter the mode (practise/exam): ")
-    if mode == 'practise':
-        App()
-    else:
-        print("To be implemented")
+choice = input("Enter the language you want to practise on: (English/Hindi): ").lower()
+if choice == 'English'.lower():
+    App()
