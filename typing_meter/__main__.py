f1e61274c46f982e8be22132b57cb9b15dc49491
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2022-04-22 19:27:19 +0530

    Working Copy without the feature of max mistyped keys

--- typing_meter/__main__.py
+++ typing_meter/__main__.py
@@ -1,11 +1,11 @@
 """typing_meter."""
 from app import App
 choice = input("Enter the language you want to practise on: (English/Hindi): ").lower()
-if choice == 'english':
-    App()
-elif choice == "hindi":
+if choice == "hindi":
     import os
     os.chdir("./typing_meter")
     print(os.getcwd())
     from subprocess import call
     call(["python", "__main__.py"])
+else:
+    App()
